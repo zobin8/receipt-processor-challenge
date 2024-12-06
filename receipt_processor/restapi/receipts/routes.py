d5c73b0d7caedcd2dae31b62e3d976c8a3009a6c
zobin8 <28766833+zobin8@users.noreply.github.com>
2024-12-06 14:51:15 -0800

    Add API responses

--- receipt_processor/restapi/receipts/routes.py
+++ receipt_processor/restapi/receipts/routes.py
@@ -13,8 +13,9 @@ api = Namespace('receipts', description='Process receipts')
 
 # Load schema
 path = os.path.join(os.path.dirname(__file__), 'schema.json')
-Item = process_schema(path, 'Item', api)
 Receipt = process_schema(path, 'Receipt', api)
+ReceiptID = process_schema(path, 'ReceiptID', api)
+Points = process_schema(path, 'Points', api)
 
 
 @api.route('/process')
@@ -22,6 +23,8 @@ class Process(Resource):
     """Submits a receipt for processing."""
 
     @api.expect(Receipt, validate=True)
+    @api.response(200, 'Returns the ID assigned to the receipt', ReceiptID)
+    @api.response(400, 'The receipt is invalid')
     def post(self) -> Any:
         """Submits a receipt for processing."""
 
@@ -30,5 +33,7 @@ class Process(Resource):
 class Points(Resource):
     """Returns the points awarded for the receipt."""
 
+    @api.response(200, 'The number of points awarded', Points)
+    @api.response(400, 'No receipt found for that id')
     def get(self, id: str) -> Any:
         """Returns the points awarded for the receipt."""
