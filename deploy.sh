a263d46246af463434d309921d3eb723fcac7a59
zobin8 <28766833+zobin8@users.noreply.github.com>
2024-12-06 16:37:42 -0800

    Add run instructions

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ python3 setup.py bdist_wheel
 
 # Build docker image
 docker build -t receipt_processor:latest -f docker/Dockerfile .
+
+docker save receipt_processor:latest -o receipt_processor_latest.tar.gz
